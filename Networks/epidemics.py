d86313d0e44ef014e67b0741293b361e977a8e92
theta-max <tomgittoes@gmail.com>
2019-06-10 08:56:20 +0100

    Added docstrings to functions

--- Networks/epidemics.py
+++ Networks/epidemics.py
@@ -4,7 +4,17 @@ import networkx as nx
 import random
 
 def SIR(G, i_rate=0.5, r_rate=1, init=1, max_time=20):
+    """Simulate an SIR epidemic on a network.
     
+    Arguments:
+    G -- A NetworkX graph object
+    
+    Keyword arguments:
+    i_rate -- the constant per-edge rate of infection (default 0.5)
+    r_rate -- the constant recovery rate of infected nodes (default 1)
+    init -- the initial number of infections (default 1)
+    max_time -- belt & braces variable to ensure loop terminates (default 20)
+    """
     # initialise network and event queue
     queue = []
     nx.set_node_attributes(G, "S", "status")
@@ -75,7 +85,20 @@ def SIR(G, i_rate=0.5, r_rate=1, init=1, max_time=20):
               inplace=True)
     return df
 
-def timeshift(df, threshold, criteria="Infected", time="time"):
-    timeshift = df[df[criteria] >= threshold][time].min()
+def timeshift(df, threshold, criterion="Infected", time="time"):
+    """Add a series of adjusted times, zeroed on a threshold number
+    of infections.
+    
+    Arguments:
+    df -- A Pandas dataframe in the format output by the SIR function
+    threshold -- an integer number of infections to be set to time zero
+    
+    Keyword arguments:
+    criterion -- the column in df containing the critrion to which the
+    threshold applies (default "Infected")
+    time -- the column in df containing the time variable to adjust (default
+    "time")
+    """
+    timeshift = df[df[criterion] >= threshold][time].min()
     df["time_adj"] = df[time] - timeshift
     return df
